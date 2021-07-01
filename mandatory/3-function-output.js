c5653427cccfb20f02266f69c2ed5365a0972531
M99ub <abozead112@outlook.com>
2021-07-01 21:22:12 +0300

    solved madatory

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,16 +1,18 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//return a float random number between 0 and 10
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//take two words word1 and word2 and merge word1 with word2
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+  var temp = firstWord.concat(secondWord);
+  return temp.concat(thirdWord);
 }
 
 /* 
